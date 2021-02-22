33c44935aee16b64447122471ad8ad0b23fdf7bf
max-student <max-student@maxtrain.com>
2021-02-22 14:40:06 -0500

    commit

---
+++ TestMathLib/MathLibExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestMathLib {
+    //class must be marked static
+    public static class MathLibExtensions {
+        //extension method with two parameters
+        public static int Power(this int i, int j){
+            var answer = 1;
+                for(var idx = 0; idx < j; idx++) {
+                answer *= i;
+            }
+            return answer;
+
+        }
+
+        // extension method
+        public static int Squared(this int i) {
+            return i * i;
+
+        }
+
+    }
+}
--- TestMathLib/Program.cs
+++ TestMathLib/Program.cs
@@ -5,13 +5,20 @@ namespace TestMathLib {
     class Program {
         static void Main(string[] args) {
             //create instance of Math Library
+
+            //extension method using two parameters
+            Console.WriteLine($"2 ^ 3 {2.Power(3)}");
+
+            //using extension method First thing we did 
+            Console.WriteLine($"3 squared is {3.Squared()}");
+
             var mathLib = new MathLib();
 
             //confirming connection with console app and library
             //Console.WriteLine($" About is {mathLib.About}");
 
 
-
+            //Test Methods in Console Project by printing
 
             Console.WriteLine($"12 + 3 {mathLib.Add(12, 3)}");
 
