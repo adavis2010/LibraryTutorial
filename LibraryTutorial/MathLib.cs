f82c8f10a12454b0b742aaa47eb61c34f45c5194
max-student <max-student@maxtrain.com>
2021-02-22 14:11:50 -0500

    Initial Commit

--- LibraryTutorial/MathLib.cs
+++ LibraryTutorial/MathLib.cs
@@ -2,7 +2,32 @@
 
 namespace LibraryTutorial {
     public class MathLib {
-        public string About { get; set; } = "About Math Lib";
+        //public string About { get; set; } = "About Math Lib";
 
+        public int Add(int i, int j) {
+            return i + j;
+        }
+        public int Subtract(int i, int j) {
+            return i - j;
+        }
+        public int Multiply(int i, int j) {
+            return i * j;
+        }
+        public int Divide(int i, int j) {
+            return i / j;
+        }
+        public int Modulo(int i, int j) {
+            return i - (i / j * j);
+
+        }
+        public int Power(int i, int j) {
+            var answer = 1;
+            for (int idx = 0; idx < j; idx++) {
+                answer *= i;
+
+            }
+            return answer;
+        }
     }
 }
+
--- TestMathLib/Program.cs
+++ TestMathLib/Program.cs
@@ -8,7 +8,26 @@ namespace TestMathLib {
             var mathLib = new MathLib();
 
             //confirming connection with console app and library
-            Console.WriteLine($" About is {mathLib.About}");
+            //Console.WriteLine($" About is {mathLib.About}");
+
+
+
+
+            Console.WriteLine($"12 + 3 {mathLib.Add(12, 3)}");
+
+            Console.WriteLine($"12 - 3 {mathLib.Subtract(12, 3)}");
+
+            Console.WriteLine($"12 * 3 {mathLib.Multiply(12, 3)}");
+
+            Console.WriteLine($"12 % 3 {mathLib.Modulo(12, 3)}");
+
+            Console.WriteLine($"12 ^ 3 {mathLib.Power(12, 3)}");
+
+
+
+
+
         }
+
     }
 }
